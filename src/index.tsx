fca114077899e0d872d053dfb18ade9a0cc2053b
Eric Kim-Butler <eric@kimbutler.xyz>
2022-01-29 12:48:04 +0100

    Init Commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import { render } from "solid-js/web";
+
+function ReactiveApp() {
+  return (
+    <>
+      <main class="flex-1"></main>
+    </>
+  );
+}
+
+render(ReactiveApp, document.getElementById("root")!);
